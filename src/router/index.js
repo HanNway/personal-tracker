11f7fc58906718eb4e3320eab3364eb583b9c43d
music-Testing <thidamya02@gmail.com>
2026-01-07 21:38:33 +0630

    Account edit and create form commit

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,25 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { auth } from '../firebase/config'
 import HomeView from '../views/HomeView.vue'
 import AuthView from '../views/AuthView.vue'
+import Accountview from '../views/Accountview.vue'
 
 
-const routes = [
+const routes = [ 
   {
     path: '/',
     name: 'home',
     component: HomeView,
     meta: { requiresAuth: true }
-  },
+  }, 
   {
     path: '/auth',
     name: 'auth',
     component: AuthView,
+  },
+  {
+    path: '/account',
+    name: 'account',
+    component: Accountview,
   },
 
 ]
